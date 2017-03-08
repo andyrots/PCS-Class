00bc34da3a687beafb6322f67015f4fc98b8581f
andrew rottenberg <andrew rottenberg>
2017-03-07 21:04:51 -0500

    add HW_71

---
+++ HW_71/products.js
@@ -0,0 +1,4 @@
+theProducts = [
+	{name: 'spoons', price: 1.99},
+	{name: 'pizza', price: 12.99}
+]
---
+++ HW_71/students.js
@@ -0,0 +1,6 @@
+
+	Joe = {fName: 'Joe', lName: 'Smith', address: '1300 Monmouth Ave', grade: 8}
+	Don1 = {fName: 'Donald', lName: 'Trump', address: '675 Princeton Ave', grade: 5}
+    James = {fName: 'James', lName: 'Jones', address: '211 Clifton Ave', grade: 7}
+    Don2 ={fName: 'Donald', lName: 'Gordon', address: '925 Forest Ave', grade: 6}
+    Arthur = {fName: 'Arthur', lName: 'Miller', address: '450 Lexington Ave', grade: 5}
